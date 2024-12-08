f5ac8877bcc116157028f17d6652591a14d3df73
gowtham207 <gowthamkannan207@gmail.com>
2024-12-08 22:32:35 +0530

    change some local storage issue

--- media_app01/src/api/api.ts
+++ media_app01/src/api/api.ts
@@ -10,27 +10,34 @@ export const AxiosObj = axios.create({
     timeout: 10000, // Optional timeout (in milliseconds)
     headers: {
       'Content-Type': 'application/json',
-      'Authorization':`Bearer ${localStorage.getItem("accesstoken")}`
     },
 }
 )
 
 
-export const getMedia = () => AxiosObj.get('media').then((res)=>res.data);
+export const getMedia = () => AxiosObj.get('media',{
+  headers:{
+     'Authorization':`Bearer ${localStorage.getItem("accesstoken")}`
+  }
+}).then((res)=>res.data);
 
-export const getContent = (id:string) => AxiosObj.get('content',{params:{
+export const getContent = (id:string|null) => AxiosObj.get('content',{params:{
   media_id:id
+},headers:{
+   'Authorization':`Bearer ${localStorage.getItem("accesstoken")}`
 }}).then((res)=>res.data);
 
 export const postLike = (id:string|null) => AxiosObj.post('/like',{
   'media_id':id
+},{
+  headers:{ 'Authorization':`Bearer ${localStorage.getItem("accesstoken")}`}
 }).then((res)=>res.data)
-
 
 export const postComment = (id:string|null , comment:string) => AxiosObj.post("/comment",{
   'media_id':id,
 'comment':comment
+},{
+  headers:{ 'Authorization':`Bearer ${localStorage.getItem("accesstoken")}`}
 }).then(res=>res.data)
 
 // export const getLikes = () => AxiosObj.get('like').then((res)=>res.data);
-
